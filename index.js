3567d9b1edb7238591e3f9be2a1a10652bcb9aa9
Victor Siret <victorsiret@gmail.com>
2018-06-20 12:58:36 +0200

    Functional search page

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
+
 //________________RENDER_PAGES____________________
 //Render Pages
 app.get('/', function (req, res) {
@@ -46,11 +47,7 @@ app.get('/discover_hike', function (req, res) {
     res.render('discover_hike', {});
 });
 
-//Render List ---> TEST PAGE
-app.get('/list', function (req, res) {
-    res.render('list', {});
-});
-//END TEST
+
 
 //_____________SUBMIT_HIKE______________
 //Post Submit Hike Page
@@ -73,68 +70,34 @@ duration:duration, difficulty:difficulty, description:description},function(err,
 });
 //_____________END_SUBMIT______________
 
-//RETRIEVE NOT WORKING!!!!
-//TEST
+
+
+//_____________RETRIEVE ALL______________
 app.get('/list', function (req, res) {
     dbh.collection("hikedata").find({}).toArray(function(err, result) {
         if (err) throw err;
         res.render('list', {hikedata:result});
       });
 });
-//END OF TEST
-//________!!!!!___________
+//_____________END_OF_TEST______________
 
-//_____________GAME______________
-//Game post
-app.post('/game', function (req, res) {
-    //Setup of Choices
-    let userChoice = req.body.userChoice;
-    let computerRand = Math.ceil(Math.random()*3)
-    
-    //Computerchoice >> Word
-    switch(computerRand) {
-        case 1:
-        computerChoice = "Rock";
-        break;
-        case 2:
-        computerChoice = "Paper";
-        break;
-        case 3:
-        computerChoice = "Scissors";
-        break;        
-    }
 
-    //Userchoice >> Number
-    switch(userChoice) {
-        case "Rock":
-        userNumb = 1;
-        break;
-        case "Paper":
-        userNumb = 2;
-        break;
-        case "Scissors":
-        userNumb = 3;
-        break;        
-    }
 
-    //Compare to find winner
-    if (userNumb === computerRand) {
-        winner = "The game is a draw.";
-    }
-    else if (userNumb - computerRand === 1||userNumb - computerRand === -2) {
-        winner = "You won!";
-    }
-    else {
-        winner = "The computer won!";
-    }
+//_____________SEARCH_PAGE______________
+app.post('/discover_hike', function (req, res) {
+    //Collect search info
+    let region = req.body.region;
+    let duration = req.body.duration;
+    let difficulty = req.body.difficulty;
+    //test
+    //res.render('discover_hike', {post:true, region:region, duration:duration, difficulty:difficulty});
 
-    //Render
-    res.render('game', {post:true, userChoice: userChoice,
-    computerChoice: computerChoice, winner: winner});
+    //Retrieve matches from Mongo
+    dbh.collection("hikedata").find({region:region, duration:duration, difficulty:difficulty}).toArray(function(err, result) {
+        if (err) throw err;
+        res.render('discover_hike', {post:true, hikedata:result});
+    })
 });
-//_____________GAME______________
-
-
 
 
 //________________USE_EXPRESS____________________
@@ -145,6 +108,7 @@ app.use(function(req, res, next){
     res.status(404).render('404', {title: "Sorry, page not found"});
 });
 
+
 //________________LISTEN_TO_PORT____________________
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(3000, () => console.log('TheHikeExchange listening on port 3000!'));
 });
